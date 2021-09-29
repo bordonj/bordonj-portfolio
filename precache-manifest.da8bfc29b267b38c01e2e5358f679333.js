dd972e067a168750be9e7fe4118f5d8263958270
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-29 10:33:46 -0700

    Updates

---
+++ precache-manifest.da8bfc29b267b38c01e2e5358f679333.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3badda1700ae7c80ea425d611ca3e100",
+    "url": "/bordonj-portfolio/index.html"
+  },
+  {
+    "revision": "6bf885e011dd5b65900b",
+    "url": "/bordonj-portfolio/static/css/2.9497e657.chunk.css"
+  },
+  {
+    "revision": "defbac8ddd38ebbb846b",
+    "url": "/bordonj-portfolio/static/css/main.60de1f05.chunk.css"
+  },
+  {
+    "revision": "6bf885e011dd5b65900b",
+    "url": "/bordonj-portfolio/static/js/2.139385b4.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/bordonj-portfolio/static/js/2.139385b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "defbac8ddd38ebbb846b",
+    "url": "/bordonj-portfolio/static/js/main.b86e7319.chunk.js"
+  },
+  {
+    "revision": "698ae3f6048513a2cede",
+    "url": "/bordonj-portfolio/static/js/runtime-main.8d1f00be.js"
+  }
+]);
